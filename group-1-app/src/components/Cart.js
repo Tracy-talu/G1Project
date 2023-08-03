51b27cb088e77f96b0ce24d89e15458ba8e6d7a0
Tracy-talu <ttmtalu@gmail.com>
2023-08-03 07:46:34 +0300

    initial commit

--- group-1-app/src/components/Cart.js
+++ group-1-app/src/components/Cart.js
@@ -1,45 +1,86 @@
-import{useState,useEffect}from "react"
+import { useState, useEffect } from "react";
 
 function Cart() {
-    const [food, setFood] = useState([]);
-  
-    useEffect(() => {
-      fetch("http://localhost:3002/food")
-        .then((response) => response.json())
-        .then((data) => setFood(data));
-    }, []);
-  
-    function remove() {
-      alert("Item removed!");
-    }
-  
-    function added() {
-      alert("Item added!");
-    }
-  
-    
-        return(
-        <>
-            
-            {food.map((food)=>(
-             
-             <div className="Container" key={food.id}>
-                <img src={food.image}></img>
-                
-                <div className="Card-body">
-                    <h5 className="card-title" >{food.food}</h5>
-                    <h5 className="card-title" >{food.price}</h5>
-                    
-                    <button onClick={added} className="btn"style={{backgroundColor:"lightblue"}}>Add To Cart</button>
-                    <button className="remove" onClick={remove} style={{backgroundColor:"lightblue",BorderRadius:"5px"}}>Remove From Cart</button>
-                </div>
-               </div>
-               
+  const [food, setFood] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3002/food")
+      .then((response) => response.json())
+      .then((data) => setFood(data));
+  }, []);
+
+  function remove(index) {
+    alert("Item removed!");
+    setCartItems(cartItems.filter((_, i) => i !== index));
+  }
+
+  function added(item) {
+    alert("Item added!");
+    setCartItems([...cartItems, item]);
+  }
+let Price=0
+  return (
+    <>
+      {food.map((food) => {
+
+  Price= food.price;
+
+  return (
+    <div className="Container" key={food.id}>
+      <div className="Card-body">
+        <img src={food.image} alt={food.food} />
+        <h5 className="card-title">{food.food}</h5>
+        <h5 className="card-title">{food.price}</h5>
+        <button
+          onClick={() => added({ item: food.food, price: food.price })}
+          className="btn"
+          style={{ backgroundColor: "lightblue" }}
+        >
+          Add To Cart
+        </button>
+        <button
+          className="remove"
+          onClick={() =>
+            remove(cartItems.findIndex((item) => item.item === food.food))
+          }
+          style={{ backgroundColor: "lightblue", borderRadius: "5px" }}
+        >
+          Remove From Cart
+        </button>
+      </div>
+    </div>
+  );
+})}
+
+
+
+      {cartItems.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Total Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item, index) => (
+              <tr key={index}>
+                <td>{item.item}</td>
+                <td>{item.price}</td>
+                <th>{Price}</th>
+                <td>
+                  <button  style={{backgroundColor:"lightblue"}} onClick={() => remove(index)}>Remove</button>
+                </td>
+              </tr>
             ))}
-             </>
-          
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+}
 
-        ) 
-}   
-
-export default Cart
+export default Cart;
